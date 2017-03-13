4642a93bb5e57468fa0392e3daeaa3b9e67a9e19
Javier Carrascal <javilumbrales@gmail.com>
2017-03-13 17:25:45 +0100

    Fix Heroku database

--- src/server/Database.js
+++ src/server/Database.js
@@ -11,9 +11,10 @@ class Database {
                 this.sequelize = new Sequelize(process.env.DATABASE_URL, {
                     dialect:  'postgres',
                     protocol: 'postgres',
-                    port:     match[4],
-                    host:     match[3],
-                    logging:  true //false
+                    logging:  true, //false
+                    dialectOptions: {
+                        ssl: true
+                    }
                 })
             } else {
                 // the application is executed on the local machine ... use mysql
